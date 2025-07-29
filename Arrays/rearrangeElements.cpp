68a27363e7fcc6e4e9054f93b87f46d30f646cdb
Aman Kushwaha <amankushwaha7284@gmail.com>
2025-07-29 16:40:46 +0530

    Rearrange array elements

---
+++ Arrays/rearrangeElements.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution
+{
+public:
+    vector<int> rearrangeArray(vector<int> &nums)
+    {
+        vector<int> ans(nums.size(), 0);
+        int odd = 1;
+        int even = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] > 0)
+            {
+                ans[even] = nums[i];
+                even += 2;
+            }
+            else
+            {
+                ans[odd] = nums[i];
+                odd += 2;
+            }
+        }
+        return ans;
+    }
+};
